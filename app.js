80cad6f3f783801bb351b740d573c6edfc5242ca
gideon-del <gideonchidi471@gmail.com>
2023-09-07 12:12:29 +0100

    Fixing time

--- app.js
+++ app.js
@@ -10,38 +10,16 @@ const arrayDay = [
   "Friday",
   "Saturday",
 ];
-let minute = date.getUTCMinutes();
-let seconds = date.getUTCSeconds();
-let hour = date.getUTCHours();
 const padString = (num) => {
   return String(num).padStart(2, 0);
 };
 const timeInterval = setInterval(() => {
-  if (seconds === 60) {
-    seconds = 0;
-    if (minute === 60) {
-      if (hour === 23) {
-        hour = 0;
-      } else {
-        hour += 1;
-      }
-      minute = 0;
-    } else {
-      minute += 1;
-    }
-  } else {
-    seconds += 1;
-  }
-  timeEl.textContent = `${padString(hour)}-${padString(minute)}-${String(
-    padString(seconds)
-  )}`;
+  timeEl.textContent = `${Date.now()}`;
 }, 1000);
 
 window.addEventListener("DOMContentLoaded", () => {
   dayEl.textContent = arrayDay[date.getDay()];
-  timeEl.textContent = `${padString(hour)}-${padString(minute)}-${String(
-    padString(seconds)
-  )}`;
+  timeEl.textContent = `${Date.now()}`;
 });
 window.addEventListener("unload", () => {
   clearInterval(timeInterval);
